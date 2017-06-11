d6799ebf2d694ecd5590ce0f17276d187f40bab2
Issac Holguin <issacholguin1@gmail.com>
2017-06-11 15:06:07 -0700

    Adds a new event successfully

--- app/src/main/java/edu/ucsb/cs/cs190i/jsegovia/getmethere/MainActivity.java
+++ app/src/main/java/edu/ucsb/cs/cs190i/jsegovia/getmethere/MainActivity.java
@@ -1,19 +1,15 @@
 package edu.ucsb.cs.cs190i.jsegovia.getmethere;
 
 import android.content.Context;
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.Toast;
 
-import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
-import com.google.android.gms.common.GooglePlayServicesRepairableException;
 import com.google.android.gms.location.places.Place;
 import com.google.android.gms.location.places.ui.PlacePicker;
 
@@ -30,7 +26,9 @@ public class MainActivity extends AppCompatActivity {
 
     private ListView lv;
     public static ArrayList<Event> events = new ArrayList<>();
-    private ArrayList<String> eventsAsStrings = new ArrayList<>();
+    public static ArrayList<String> eventsAsStrings = new ArrayList<>();
+    public static Place place;
+    public static ArrayAdapter<String> arrayAdapter;
 
 
 
@@ -53,7 +51,7 @@ public class MainActivity extends AppCompatActivity {
 
 
         lv = (ListView) findViewById(R.id.myList);
-        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, eventsAsStrings);
+        arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, eventsAsStrings);
 
 
 
@@ -65,27 +63,13 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-/*
+
                 eventFragment ef = new eventFragment();
                 ef.show(getFragmentManager(), "Random");
 
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
-                        */
-                int PLACE_PICKER_REQUEST = 1;
 
-
-                try {
-                    startActivityForResult(builder.build(MainActivity.this), PLACE_PICKER_REQUEST);
-                } catch (GooglePlayServicesRepairableException e) {
-                    e.printStackTrace();
-                    System.out.println("This one");
-                    Log.d("This one", "THis one ");
-                } catch (GooglePlayServicesNotAvailableException e) {
-                    e.printStackTrace();
-                    System.out.println("no This one");
-                    Log.d("no This one", "no THis one ");
-                }
 
 
             }
@@ -100,21 +84,6 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    public void startLocationIntent() throws GooglePlayServicesNotAvailableException, GooglePlayServicesRepairableException {
-        int PLACE_PICKER_REQUEST = 1;
-        PlacePicker.IntentBuilder builder = new PlacePicker.IntentBuilder();
-        startActivityForResult(builder.build(MainActivity.this), PLACE_PICKER_REQUEST);
-    }
 
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        int PLACE_PICKER_REQUEST = 1;
-        if (requestCode == PLACE_PICKER_REQUEST) {
-            if (resultCode == RESULT_OK) {
-                Place place = PlacePicker.getPlace(data, this);
-                String toastMsg = String.format("Place: %s", place.getName());
-                Toast.makeText(this, toastMsg, Toast.LENGTH_LONG).show();
-            }
-        }
-    }
 
 }
--- app/src/main/java/edu/ucsb/cs/cs190i/jsegovia/getmethere/eventFragment.java
+++ app/src/main/java/edu/ucsb/cs/cs190i/jsegovia/getmethere/eventFragment.java
@@ -1,8 +1,32 @@
 package edu.ucsb.cs.cs190i.jsegovia.getmethere;
 
 import android.app.DialogFragment;
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.annotation.RequiresApi;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TimePicker;
+import android.widget.Toast;
+
+import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
+import com.google.android.gms.common.GooglePlayServicesRepairableException;
+import com.google.android.gms.location.places.ui.PlacePicker;
+
+import java.sql.Time;
+import java.util.ArrayList;
+
+import static android.app.Activity.RESULT_OK;
+import static edu.ucsb.cs.cs190i.jsegovia.getmethere.MainActivity.PLACE_PICKER_REQUEST;
+import static edu.ucsb.cs.cs190i.jsegovia.getmethere.MainActivity.arrayAdapter;
+import static edu.ucsb.cs.cs190i.jsegovia.getmethere.MainActivity.events;
+import static edu.ucsb.cs.cs190i.jsegovia.getmethere.MainActivity.eventsAsStrings;
+import static edu.ucsb.cs.cs190i.jsegovia.getmethere.MainActivity.place;
 
 
 public class eventFragment extends DialogFragment {
@@ -12,32 +36,89 @@ public class eventFragment extends DialogFragment {
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
     }
-/*
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
 
-        View view = inflater.inflate(R.layout.event_fragment, container, false);
+      final View view = inflater.inflate(R.layout.event_fragment, container, false);
+
+
 
         Button location = (Button) view.findViewById(R.id.location);
+
+        final EditText activity = (EditText) view.findViewById(R.id.EventName);
+        final EditText eventLocation = (EditText) view.findViewById(R.id.eventLocation);
+
         location.setOnClickListener(new View.OnClickListener() {
             @RequiresApi(api = Build.VERSION_CODES.M)
             @Override
             public void onClick(View v) {
+                PlacePicker.IntentBuilder builder = new PlacePicker.IntentBuilder();
+                try {
+                    startActivityForResult(builder.build(getActivity()), PLACE_PICKER_REQUEST);
+                } catch (GooglePlayServicesRepairableException e) {
+                    e.printStackTrace();
+                } catch (GooglePlayServicesNotAvailableException e) {
+                    e.printStackTrace();
+                }
 
-                    PlacePicker.IntentBuilder builder = new PlacePicker.IntentBuilder();
-                    try {
-                        startActivityForResult(builder.build(getActivity()), MainActivity.PLACE_PICKER_REQUEST);
-                    } catch (GooglePlayServicesRepairableException e) {
-                        e.printStackTrace();
-                    } catch (GooglePlayServicesNotAvailableException e) {
-                        e.printStackTrace();
-                    }
             }
         });
 
+
+
+
+
+
+
+        Button add = (Button) view.findViewById(R.id.ADD);
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                TimePicker starter = (TimePicker) view.findViewById(R.id.StartTime);
+                TimePicker ender = (TimePicker) view.findViewById(R.id.EndTime);
+
+                Time startTime = new Time(starter.getHour(),starter.getMinute(),0);
+                Time endTime = new Time(ender.getHour(),ender.getMinute(),0);
+
+                Event e = new Event(activity.getText().toString(), eventLocation.getText().toString(), startTime, endTime);
+                e.setEventLat(place.getLatLng().latitude);
+                e.setEventLng(place.getLatLng().longitude);
+
+                events.add(e);
+                upDateStringsList(events, eventsAsStrings);
+                arrayAdapter.notifyDataSetChanged();
+
+
+            }
+
+            private void upDateStringsList(ArrayList<Event> events, ArrayList<String> eventsAsStrings) {
+                eventsAsStrings.clear();
+                for (int i = 0; i < events.size(); i++) {
+                    eventsAsStrings.add(new String(events.get(i).getName() + " at " + events.get(i).getLocation() +
+                            "        Time: " + events.get(i).getEventStart().toString() + " - " + events.get(i).getEventEnd().toString()));
+                }
+            }
+
+        });
+
+
+
+
         return view;
 
-    */
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == PLACE_PICKER_REQUEST) {
+            if (resultCode == RESULT_OK) {
+                place = PlacePicker.getPlace(data, getActivity());
+                String toastMsg = String.format("Place: %s", place.getName());
+                Toast.makeText(getActivity(), toastMsg, Toast.LENGTH_LONG).show();
+            }
+        }
+    }
 
 }
